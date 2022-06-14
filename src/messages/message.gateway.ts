e501a9346f1a42e1b12da1a493b81686bd4e464d
babinm1h <babinmih@mail.ru>
2022-06-14 17:29:47 +0300

    dpl

--- src/messages/message.gateway.ts
+++ src/messages/message.gateway.ts
@@ -6,7 +6,7 @@ import { CreateMessageDto } from "./dtos/createMessage.dto";
 import { MessagesService } from "./messages.service";
 
 
-@WebSocketGateway(+process.env.PORT || 80, { cors: true })
+@WebSocketGateway(+process.env.PORT || 80, { cors: true, transports: "polling" })
 export class MessageGateway {
 
     constructor(
